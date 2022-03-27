26c8bd6bb62de1ffd47ec69ebeeb9badcb7ee479
Daniel Mafra <danielmafradev@gmail.com>
2022-03-27 15:39:02 -0300

    feat: add reducers to GlobalContext

--- src/contexts/GlobalContext.tsx
+++ src/contexts/GlobalContext.tsx
@@ -1,8 +1,16 @@
 import React, { createContext, useReducer } from "react";
 import { reducerActionType } from "../types/reducerActionType";
+import { RegisterType, registerInitialState, registerReducer } from "../reducers/registerReducer";
 import { ThemeType, themeInitialState, themeReducer } from "../reducers/themeReducer";
+import { MenuType, menuInitialState, menuReducer } from "../reducers/menuReducer";
+import { TableRegistersType, tableRegistersInitialState, tableRegistersReducer } from "../reducers/tableRegistersReducer";
+import { ModalRegistersType, modalRegistersInitialState, modalRegistersReducer } from "../reducers/modalRegistersReducer";
 
 type initialStateType = {
+  register: RegisterType;
+  tableRegisters: TableRegistersType;
+  modalRegisters: ModalRegistersType;
+  menu: MenuType;
   theme: ThemeType;
 }
 
@@ -12,6 +20,10 @@ type GlobalContextType = {
 }
 
 const initialState = {
+  register: registerInitialState,
+  tableRegisters: tableRegistersInitialState,
+  modalRegisters: modalRegistersInitialState,
+  menu: menuInitialState,
   theme: themeInitialState
 }
 
@@ -21,6 +33,10 @@ export const GlobalContext = createContext<GlobalContextType>({
 });
 
 const mainReducer = (state: initialStateType, action: reducerActionType) => ({
+  register: registerReducer(state.register, action),
+  tableRegisters: tableRegistersReducer(state.tableRegisters, action),
+  modalRegisters: modalRegistersReducer(state.modalRegisters, action),
+  menu: menuReducer(state.menu, action),
   theme: themeReducer(state.theme, action)
 });
 
