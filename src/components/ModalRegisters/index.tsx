536abda98335da01b1e109d0571a006a74411434
Daniel Mafra <danielmafradev@gmail.com>
2022-03-27 16:02:05 -0300

    feat: create ModalRegisters component

---
+++ src/components/ModalRegisters/index.tsx
@@ -0,0 +1,185 @@
+import { useContext, useEffect, useRef } from 'react';
+import { GlobalContext } from '../../contexts/GlobalContext';
+import { useApi } from '../../hooks/useApi';
+import { useCleanup } from '../../hooks/useCleanup';
+import { ThemeContext } from 'styled-components';
+import * as C from './styles';
+import { Close } from '@material-ui/icons';
+
+import Loading from '../Loading';
+import Error from '../Error';
+
+type ModalRegistersProps = {
+  endpoint: string;
+  type: string;
+  dataKey: string;
+  title: string;
+  children: JSX.Element;
+}
+
+const ModalRegisters = ({ endpoint, type, dataKey, title, children }: ModalRegistersProps) => {
+  const api = useApi();
+  const {
+    clearOnAnyModalRegisterClosed,
+    clearOnRegisterUpdateCancel,
+    clearOnRegisterDeleteCancel,
+    clearOnRegisterCreated,
+    clearOnRegisterUpdated,
+    clearOnRegisterDeleted
+  } = useCleanup();
+  const { state, dispatch } = useContext(GlobalContext);
+  const { modal_registers } = useContext(ThemeContext);
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  const handleCancel = () => {
+    clearOnAnyModalRegisterClosed();
+    clearOnRegisterUpdateCancel();
+    clearOnRegisterDeleteCancel();
+  }
+
+  const handleSubmit = async () => {
+    if (state.register.isReady) {
+      dispatch({
+        type: 'REGISTER_CHANGE_ISSENDING',
+        payload: {
+          isSending: true
+        }
+      });
+      let result;
+      if (type === 'update') {
+        result = await api.updateRegister(endpoint, state.modalRegisters.registerEditingId, state.register.props);
+      } else if (type === 'create') {
+        result = await api.createRegister(endpoint, state.register.props);
+      }
+      //const result = await api.createRegister(endpoint, state.register.props);
+      if (result[dataKey]) {
+        clearOnAnyModalRegisterClosed();
+        if (type === 'create') {
+          clearOnRegisterCreated();
+        } else if (type === 'update') {
+          clearOnRegisterUpdated();
+        }
+      } else if (result.error) {
+        dispatch({
+          type: 'REGISTER_CHANGE_ERROR',
+          payload: {
+            error: result.error
+          }
+        });
+        dispatch({
+          type: 'REGISTER_CHANGE_HASERROR',
+          payload: {
+            hasError: true
+          }
+        });
+      } else {
+        dispatch({
+          type: 'REGISTER_CHANGE_ERROR',
+          payload: {
+            error: 'Ocorreu um erro.'
+          }
+        });
+        dispatch({
+          type: 'REGISTER_CHANGE_HASERROR',
+          payload: {
+            hasError: true
+          }
+        });
+      }
+    } else {
+      dispatch({
+        type: 'REGISTER_CHANGE_HASERROR',
+        payload: {
+          hasError: true
+        }
+      });
+    }
+    dispatch({
+      type: 'REGISTER_CHANGE_ISSENDING',
+      payload: {
+        isSending: false
+      }
+    });
+  }
+
+  const handleDelete = async () => {
+    const result = await api.deleteRegister(endpoint, state.modalRegisters.registerEditingId);
+    if (result.status) {
+      clearOnAnyModalRegisterClosed();
+      clearOnRegisterDeleted();
+    }
+  }
+
+  useEffect(() => {
+    const clickedOutside = (e: MouseEvent) => {
+      if (state.modalRegisters.openedModal && ref.current && !ref.current.contains(e.target as HTMLDivElement)) {
+        dispatch({
+          type: 'MODALREGISTERS_SET_OPENEDMODAL',
+          payload: {
+            openedModal: false
+          }
+        });
+      }
+    }
+
+    document.addEventListener("mousedown", clickedOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", clickedOutside);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [state.modalRegisters.openedModal]);
+
+  return (
+    <C.ModalContainer>
+      <C.ModalContent className="onEnterStoreModal" ref={ref}>
+        <C.ModalHeader>
+          <C.ModalTitle>{title}</C.ModalTitle>
+          <C.ModalCloseButton
+            disabled={state.register.isSending}
+            onClick={handleCancel}
+          >
+            <Close style={{ color: modal_registers.font, fontSize: '24px' }} />
+          </C.ModalCloseButton>
+        </C.ModalHeader>
+
+        {state.modalRegisters.loadingRegister ? (
+          <Loading />
+        ) : (
+          <C.Form>
+            {children}
+          </C.Form>
+        )}
+
+        {state.register.hasError
+          &&
+          <Error>
+            <span>{state.register.error}</span>
+          </Error>
+        }
+
+        <C.ButtonArea>
+          {!state.modalRegisters.loadingRegister
+            &&
+            <C.Button
+              className={`${state.modalRegisters.deletingRegister ? 'buttonDelete' : 'buttonSave'}`}
+              disabled={state.register.isSending}
+              onClick={state.modalRegisters.deletingRegister ? handleDelete : handleSubmit}
+            >
+              {state.modalRegisters.deletingRegister ? 'Excluir' : 'Salvar'}
+            </C.Button>}
+          <C.Button
+            className="buttonCancel"
+            disabled={state.register.isSending}
+            onClick={handleCancel}
+          >
+            Cancelar
+          </C.Button>
+        </C.ButtonArea>
+
+      </C.ModalContent>
+    </C.ModalContainer>
+  );
+}
+
+export default ModalRegisters;
---
+++ src/components/ModalRegisters/styles.ts
@@ -0,0 +1,137 @@
+import styled from 'styled-components';
+
+export const ModalContainer = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100vh;
+  width: 100vw;
+  background-color: rgba(0,0,0,0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ModalContent = styled.div`
+  background-color: ${props => props.theme.modal_registers.background};
+  padding: 16px;
+  border-radius: 8px;
+  width: 500px;
+  max-width: 90%;
+  overflow-y: auto;
+  max-height: 90%
+`;
+
+export const ModalHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-bottom: 16px;
+  margin-bottom: 32px;
+  border-bottom: 1px solid ${props => props.theme.modal_registers.separator};
+`;
+
+export const ModalTitle = styled.h4`
+  color: ${props => props.theme.modal_registers.font};
+  font-size: 18px;
+`;
+
+export const ModalCloseButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: transparent;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  &:disabled {
+    opacity: 0.5;
+    cursor: initial;
+  }
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
+
+export const InputContainer = styled.div`
+  flex: 1;
+`;
+
+export const InputTitle = styled.span`
+  font-weight: 500;
+  display: block;
+  margin-bottom: 8px;
+  color: ${props => props.theme.modal_registers.font};
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  padding: 8px 16px;
+  border-radius: 4px;
+  border: 1px solid ${props => props.theme.modal_registers.input_border};
+  background-color: ${props => props.theme.modal_registers.input_background};
+  color: ${props => props.theme.modal_registers.input_font};
+  outline: none;
+  ::placeholder {
+    font-style: italic;
+  }
+`;
+
+export const Select = styled.select`
+  width: 100%;
+  padding: 7px 16px;
+  border-radius: 4px;
+  border: 1px solid rgba(0,0,0,0.3);
+  outline: none;
+`;
+
+export const Option = styled.option``;
+
+export const ButtonArea = styled.div`
+  display: flex;
+  gap: 8px;
+  padding-top: 16px;
+  margin-top: 32px;
+  border-top: 1px solid ${props => props.theme.modal_registers.separator};
+`;
+
+export const Button = styled.button`
+  width: 142px;
+  min-height: 32px;
+  color: #ffffff;
+  font-size: 14px;
+  font-weight: bold;
+  padding: 8px 12px;
+  border: none;
+  outline: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: all .3s ease;
+  &:disabled {
+    opacity: 0.5;
+    cursor: initial;
+  }
+  &.buttonSave {
+    background-color: #2eb85c;
+  }
+  &.buttonSave:hover {
+    background-color:rgb(46,164,92);
+  }
+  &.buttonCancel {
+    background-color: rgba(0,0,0,0.3);
+  }
+  &.buttonCancel:hover {
+    background-color: rgba(0,0,0,0.45);
+  }
+  &.buttonDelete {
+    background-color: ${props => props.theme.table_registers.delete_button_background};
+  }
+  &.buttonDelete:hover {
+    background-color: ${props => props.theme.table_registers.delete_button_background_hover};
+  }
+`;
