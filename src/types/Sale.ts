8bf3572a8c6050111492b474f6d2a2d5a8d23a15
Daniel Mafra <danielmafradev@gmail.com>
2022-03-27 15:46:14 -0300

    feat: create Sale types

---
+++ src/types/Sale.ts
@@ -0,0 +1,34 @@
+export type Sale = {
+  id: string;
+  product_id: string;
+  product_name: string;
+  product_description: string;
+  unity_id: string;
+  unity: string
+  client_id: string;
+  client_name: string;
+  client_city: string;
+  client_state: string;
+  seller_id: string;
+  seller: string;
+  carrier_id: string;
+  carrier: string;
+  status: number;
+}
+
+export type CreateSale = {
+  product: string;
+  unity: string;
+  client: string;
+  seller: string;
+  carrier: string;
+}
+
+export type UpdateSale = {
+  product?: string;
+  unity?: string;
+  client?: string;
+  seller?: string;
+  carrier?: string;
+  status?: number;
+}
