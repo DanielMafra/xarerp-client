96fe55087bb5f082bf028ec6b9676aa9d9218332
Daniel Mafra <danielmafradev@gmail.com>
2022-03-27 15:20:33 -0300

    feat: create useApi hook

---
+++ src/hooks/useApi.ts
@@ -0,0 +1,55 @@
+import api from '../services/axios';
+
+export const useApi = () => ({
+
+  /*===== Register / Tables =====*/
+
+  fetchAllData: async (endpoint: string, currentPage: number, query: string = '') => {
+    const response = await api.get(`/v1/${endpoint}/?page=${currentPage}&q=${query}`);
+    return response.data;
+  },
+
+  createRegister: async (endpoint: string, data: Object) => {
+    const response = await api.post(`/v1/${endpoint}`, { ...data })
+      .catch((error) => {
+        return error.response;
+      })
+    return response.data;
+  },
+
+  updateRegister: async (endpoint: string, id: string, data: Object) => {
+    const response = await api.put(`/v1/${endpoint}/${id}`, { ...data })
+      .catch((error) => {
+        return error.response;
+      })
+    return response.data;
+  },
+
+  deleteRegister: async (endpoint: string, id: string) => {
+    const response = await api.delete(`/v1/${endpoint}/${id}`)
+      .catch((error) => {
+        return error.response;
+      })
+    return response.data;
+  },
+
+  /*===== Authentication =====*/
+
+  validateToken: async (token: string) => {
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    const response = await api.post('/validate');
+    return response.data;
+  },
+
+  signin: async (email: string, password: string) => {
+    const response = await api.post('/login', { email, password });
+    return response.data;
+  },
+
+  logout: async () => {
+    return {
+      status: true
+    }
+  },
+
+});
