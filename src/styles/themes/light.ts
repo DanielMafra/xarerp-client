f408de19d081a192e68d8c0c72708f8b83f63135
Daniel Mafra <danielmafradev@gmail.com>
2022-03-27 15:27:15 -0300

    feat: create light theme styles

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,66 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  colors: {
+    danger: '#e55353',
+    font_default: '#374253',
+    headerBg: '#ffffff',
+    contentBg: '#ecedef',
+  },
+  inputs: {
+    background: '#ffffff',
+    border: 'rgba(0,0,0,0.09)',
+    text: '#7e808f'
+  },
+  login: {
+    title: '#2c384af2',
+    description: '#2c384aae',
+    background: '#ffffff',
+    button: '#321fdb',
+    button_hover: '#5141e0'
+  },
+  menu: {
+    background: '#3c4b64',
+    active_border: 'rgba(255,255,255,0.5)',
+    active_background: 'rgba(255,255,255,0.05)',
+    icon: '#ffffff',
+    font: '#ffffff',
+  },
+  user_menu: {
+    icon: '#374253',
+    menu_background: '#ebedef',
+    font_title: '#8a93a2',
+    font_item: '#4f5d73',
+    links_background: '#ffffff'
+  },
+  notifications: {
+    icon: '#374253'
+  },
+  switcher: {
+    background: '#321fdb',
+    hover: '#5141e0',
+    active: '#5b4ce2',
+    icon: '#ffffff'
+  },
+  table_registers: {
+    background: '#ffffff',
+    title_table: '#768192',
+    item_background_one: '#f9fafa',
+    item_background_two: '#ecedee',
+    item_font_color: 'rgba(44, 56, 74, 0.95)',
+    edit_button_background: '#39f',
+    edit_button_background_hover: '#52a8ff',
+    delete_button_background: '#e55353',
+    delete_button_background_hover: '#e96d6d'
+  },
+  modal_registers: {
+    background: '#ffffff',
+    separator: '#d8dbe0',
+    font: '#2c384af2',
+    input_font: 'rgba(44,56,74,0.95)',
+    input_background: '#ffffff',
+    input_border: '#b1b7c1'
+  },
+  icons: {
+    search: 'url("/assets/search-light.svg")'
+  }
+};
