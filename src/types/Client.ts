f10202fc124eebf53d82ca903e9fca81b3d4b68a
Daniel Mafra <danielmafradev@gmail.com>
2022-03-27 15:42:37 -0300

    feat: create Client types

---
+++ src/types/Client.ts
@@ -0,0 +1,34 @@
+export type Client = {
+  id: string;
+  name: string;
+  email: string;
+  tel: string;
+  cep: number;
+  city: string;
+  state: string;
+  user_id: string;
+  user: string;
+  unity_id: string;
+  unity: string;
+}
+
+export type CreateClient = {
+  name: string;
+  email: string;
+  tel: string;
+  cep: number;
+  city: string;
+  state: string;
+  unity: string;
+}
+
+export type UpdateClient = {
+  name?: string;
+  email?: string;
+  tel?: string;
+  cep?: number;
+  city?: string;
+  state?: string;
+  user?: string;
+  unity?: string;
+}
