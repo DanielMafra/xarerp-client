d508027db11f2a1ae4ac9b44ef22dc477a4ee7bc
Daniel Mafra <danielmafradev@gmail.com>
2022-03-27 15:48:25 -0300

    create UserCreate types

---
+++ src/types/UserCreate.ts
@@ -0,0 +1,38 @@
+export type User = {
+  id: string;
+  name: string;
+  email: string;
+  unity_id: string;
+  unity: string;
+  position: string;
+  permissions: string;
+  active: boolean;
+}
+
+export type CreateUser = {
+  id: string;
+  name: string;
+  email: string;
+  password?: string;
+  unity_id: string;
+  position: string;
+  permissions: string;
+}
+
+export type NewCreateUser = {
+  name: string;
+  email: string;
+  unity: string;
+  position: string;
+  permissions: string;
+}
+
+export type UpdateUser = {
+  name?: string;
+  email?: string;
+  password?: string;
+  unity?: string;
+  position?: string;
+  permissions?: string;
+  active?: boolean;
+}
